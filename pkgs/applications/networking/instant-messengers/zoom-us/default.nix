d7a68a455ae0d6fb1bf654d43216e2ed5e22db4c
Tobias Widmann <widmann@fisalis.de>
2024-07-28 15:50:55 +0200

    zoom-us: work around os-release check, provide /usr/libexec/xdg-desktop-portal

--- pkgs/applications/networking/instant-messengers/zoom-us/default.nix
+++ pkgs/applications/networking/instant-messengers/zoom-us/default.nix
@@ -38,6 +38,9 @@
 , util-linux
 , pulseaudioSupport ? true
 , libpulseaudio
+, xdg-desktop-portal
+, bubblewrap
+, bash
 }:
 
 let
@@ -177,13 +180,29 @@ stdenv.mkDerivation rec {
     # everybody runs Zoom only after cd to Zoom package directory? Anyway, :facepalm:
     # Clear Qt paths to prevent tripping over "foreign" Qt resources.
     # Clear Qt screen scaling settings to prevent over-scaling.
-    makeWrapper $out/opt/zoom/ZoomLauncher $out/bin/zoom \
+    makeWrapper $out/opt/zoom/ZoomLauncher $out/opt/zoom/.naked-zoom \
       --chdir "$out/opt/zoom" \
       --unset QML2_IMPORT_PATH \
       --unset QT_PLUGIN_PATH \
       --unset QT_SCREEN_SCALE_FACTORS \
       --prefix PATH : ${lib.makeBinPath [ coreutils glib.dev pciutils procps util-linux ]} \
       --prefix LD_LIBRARY_PATH ":" ${libs}
+
+    cat <<-EOF > "$out/fake-os-release"
+    NAME="Arch Linux"
+    PRETTY_NAME="Arch Linux"
+    ID=arch
+    BUILD_ID=rolling
+    ANSI_COLOR="38;2;23;147;209"
+    HOME_URL="https://archlinux.org/"
+    DOCUMENTATION_URL="https://wiki.archlinux.org/"
+    EOF
+
+    cat <<-EOF > "$out/bin/zoom"
+    #!${bash}/bin/bash
+    exec "${bubblewrap}/bin/bwrap" --dev-bind / / --ro-bind "$out/fake-os-release" "\$(realpath /etc/os-release)" --tmpfs /usr --ro-bind /usr/bin /usr/bin --symlink "${xdg-desktop-portal}/libexec/xdg-desktop-portal" /usr/libexec/xdg-desktop-portal "$out/opt/zoom/.naked-zoom" "\$@"
+    EOF
+    chmod +x "$out/bin/zoom"
 
     # Backwards compatibility: we used to call it zoom-us
     ln -s $out/bin/{zoom,zoom-us}
