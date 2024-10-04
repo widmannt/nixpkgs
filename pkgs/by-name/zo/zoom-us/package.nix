b5161b86f983f43bf1ca8742962b2767295461ec
Tobias Widmann <widmann@fisalis.de>
2024-10-04 13:29:57 +0200

    zoom-us: provide /usr/libexec/xdg-desktop-portal

--- pkgs/by-name/zo/zoom-us/package.nix
+++ pkgs/by-name/zo/zoom-us/package.nix
@@ -39,6 +39,9 @@
 , pulseaudioSupport ? true
 , libpulseaudio
 , pulseaudio
+, xdg-desktop-portal
+, bubblewrap
+, bash
 }:
 
 let
@@ -175,7 +178,7 @@ stdenv.mkDerivation rec {
     # everybody runs Zoom only after cd to Zoom package directory? Anyway, :facepalm:
     # Clear Qt paths to prevent tripping over "foreign" Qt resources.
     # Clear Qt screen scaling settings to prevent over-scaling.
-    makeWrapper $out/opt/zoom/ZoomLauncher $out/bin/zoom \
+    makeWrapper $out/opt/zoom/ZoomLauncher $out/opt/zoom/.naked-zoom \
       --chdir "$out/opt/zoom" \
       --unset QML2_IMPORT_PATH \
       --unset QT_PLUGIN_PATH \
@@ -188,6 +191,12 @@ stdenv.mkDerivation rec {
       --unset QT_PLUGIN_PATH \
       --unset QT_SCREEN_SCALE_FACTORS \
       --prefix LD_LIBRARY_PATH ":" ${libs}
+
+    cat <<-EOF > "$out/bin/zoom"
+    #!${bash}/bin/bash
+    exec "${bubblewrap}/bin/bwrap" --dev-bind / / --tmpfs /usr --ro-bind /usr/bin /usr/bin --symlink "${xdg-desktop-portal}/libexec/xdg-desktop-portal" /usr/libexec/xdg-desktop-portal "$out/opt/zoom/.naked-zoom" "\$@"
+    EOF
+    chmod +x "$out/bin/zoom"
 
     # Backwards compatibility: we used to call it zoom-us
     ln -s $out/bin/{zoom,zoom-us}
