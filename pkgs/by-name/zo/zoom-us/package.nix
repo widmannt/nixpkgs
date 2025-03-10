7384613aa209ae464482634a33e9f8ff15057b21
Tobias Widmann <widmann@fisalis.de>
2025-03-10 22:55:08 +0100

    zoom-us: provide xdg-desktop-portal

--- pkgs/by-name/zo/zoom-us/package.nix
+++ pkgs/by-name/zo/zoom-us/package.nix
@@ -41,6 +41,9 @@
   libpulseaudio,
   pulseaudio,
   callPackage,
+  xdg-desktop-portal,
+  bubblewrap,
+  bash,
 }:
 
 let
@@ -204,13 +207,19 @@ stdenv.mkDerivation {
       # everybody runs Zoom only after cd to Zoom package directory? Anyway, :facepalm:
       # Clear Qt paths to prevent tripping over "foreign" Qt resources.
       # Clear Qt screen scaling settings to prevent over-scaling.
-      makeWrapper $out/opt/zoom/ZoomLauncher $out/bin/zoom \
+      makeWrapper $out/opt/zoom/ZoomLauncher $out/opt/zoom/.naked-zoom \
         --chdir "$out/opt/zoom" \
         --unset QML2_IMPORT_PATH \
         --unset QT_PLUGIN_PATH \
         --unset QT_SCREEN_SCALE_FACTORS \
         --prefix PATH : ${binPath} \
         --prefix LD_LIBRARY_PATH ":" ${libs}
+
+      cat <<-EOF > "$out/bin/zoom"
+      #!${bash}/bin/bash
+      exec "${bubblewrap}/bin/bwrap" --dev-bind / / --tmpfs /usr --ro-bind /usr/bin /usr/bin --symlink "${xdg-desktop-portal}/libexec/xdg-desktop-portal" /usr/libexec/xdg-desktop-portal --symlink "\''$(dirname -- "\''$NIX_XDG_DESKTOP_PORTAL_DIR")" /usr/share/xdg-desktop-portal "$out/opt/zoom/.naked-zoom" "\''$@"
+      EOF
+      chmod +x "$out/bin/zoom"
 
       if [ -f $out/opt/zoom/ZoomWebviewHost ]; then
         wrapProgram $out/opt/zoom/ZoomWebviewHost \
